374b7206e7f257c4fe276022d756bbaa951a3d75
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2024-08-23 16:14:29 +0100

    Initial commit

---
+++ build_local.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Clear out any firmware files
+rm --force *.hex
+rm --force *.uf2
+
+# Ensure QMK CLI is installed
+if ! command -v qmk >/dev/null 2>&1; then
+    errcho "QMK CLI not found"
+    errcho "Install with pipx install qmk"
+    exit 2
+fi
+
+# Checkout QMK repository
+if [ -d "vial-qmk" ]; then
+    echo "VIAL QMK repository already exists, pulling latest changes"
+    cd vial-qmk
+    git pull
+    cd ..
+else
+    echo "VIAL QMK repository does not exist, cloning"
+    git clone --depth 1 --recurse-submodules https://github.com/vial-kb/vial-qmk
+fi
+
+# Copy files to QMK repository
+if [ ! -L "vial-qmk/keyboards/crkbd/keymaps/notatutorial" ]; then
+    echo "Symlink keymaps in VIAL QMK repository"
+    ln --symbolic --force --no-dereference $(pwd)/crkbd/notatutorial vial-qmk/keyboards/crkbd/keymaps/
+fi
+if [ ! -L "vial-qmk/keyboards/ferris/sweep/keymaps/notatutorial" ]; then
+    echo "Symlink keymaps in VIAL QMK repository"
+    ln --symbolic --force --no-dereference $(pwd)/ferris/sweep/notatutorial vial-qmk/keyboards/ferris/sweep/keymaps/
+fi
+
+# Build firmware
+echo "Building firmware..."
+cd vial-qmk
+qmk compile -kb crkbd/r2g -km notatutorial
+qmk compile -kb crkbd/rev1 -km notatutorial
+qmk compile -kb crkbd/rev1 -km notatutorial -e CONVERT_TO=kb2040
+qmk compile -kb ferris/sweep -km notatutorial
+cd ..
+
+# Copy firmware out of build directory
+cp vial-qmk/.build/*.hex .
+cp vial-qmk/.build/*.uf2 .
---
+++ ferris/sweep/notatutorial/config.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+/* Select hand configuration */
+#define MASTER_LEFT
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur.
+
+#define VIAL_KEYBOARD_UID {0x47, 0x97, 0x7E, 0x32, 0xC9, 0xAC, 0x13, 0xF3}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 16
+
+// Config to make MOD TAP keys more responsive
+#define PERMISIVE_HOLD
+
+// Disable some of the features that we do not use to save space
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_MUSIC_MODE
+#define LAYER_STATE_8BIT
